d4ecf5d4ff666af8122b35b858ce6e2e675ebebe
Moses <lazaromoses77@gmail.com>
2020-02-29 16:41:28 +0300

    Updates

---
+++ precache-manifest.05e18f627c240e1ed04b89273069e6c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e894fb4131b489be8a46e26149e3f579",
+    "url": "/New-World-App/index.html"
+  },
+  {
+    "revision": "ccc913c29f476520cfb1",
+    "url": "/New-World-App/static/css/2.38a4df54.chunk.css"
+  },
+  {
+    "revision": "85235954a7ddba2a578d",
+    "url": "/New-World-App/static/css/main.4c5c5e08.chunk.css"
+  },
+  {
+    "revision": "ccc913c29f476520cfb1",
+    "url": "/New-World-App/static/js/2.43457eaf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/New-World-App/static/js/2.43457eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85235954a7ddba2a578d",
+    "url": "/New-World-App/static/js/main.80cbb684.chunk.js"
+  },
+  {
+    "revision": "b14a504ce3c044f602c8",
+    "url": "/New-World-App/static/js/runtime-main.1a97497e.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/New-World-App/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/New-World-App/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/New-World-App/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "eb93bdbfe9281cef498860daf465c71b",
+    "url": "/New-World-App/static/media/study.eb93bdbf.jpg"
+  }
+]);
